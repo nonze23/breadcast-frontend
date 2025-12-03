29d2b5c87f7dc7a6af667d8e3db0346cb3a0f16a
jiwon Lee <dlwleasy1@gmail.com>
2025-12-03 23:44:37 +0900

    [fix-auth]proxy설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // '/api'라는 주소로 요청이 오면 프록시가 작동합니다.
+      "/api": {
+        target: "http://43.200.233.19", // 실제 백엔드 서버 주소
+        changeOrigin: true, // 서버가 요청의 출처를 자신의 도메인으로 인식하게 함 (CORS 해결 핵심)
+        rewrite: (path) => path.replace(/^\/api/, ""), // 요청 경로에서 '/api'를 제거하고 백엔드로 보냄
+        secure: false, // https가 아닌 http를 쓸 경우 false 설정
+      },
+    },
+  },
+});
